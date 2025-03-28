7ad31b145a02ada8733a05c3afff221899325f63
livianegrini <livia.negrini@sou.inteli.edu.br>
2025-03-28 20:04:05 -0300

    Lista 2 adicionada

---
+++ Aula07/teste.js
@@ -0,0 +1,43 @@
+// Versão 1 da função de análise de crédito
+function analisarCredito1() {
+  var compras = [2500, 1200, 800, 100];
+  var totalCompras = compras[0];
+  var limite = 5000;
+  var status = 'aprovado';
+  var saldoDisponivel = 0;
+  var i = 1;
+
+  do {
+      totalCompras += compras[i];
+      i++;
+  } while (limite >= totalCompras && i < compras.length);
+
+  saldoDisponivel = limite - totalCompras;
+
+  if (saldoDisponivel < 0) {
+      status = 'negado';
+  }
+  console.log(`Seu crédito foi ${status}. Saldo disponível: ${saldoDisponivel}.`);
+}
+
+// Versão 2 da função de análise de crédito
+function analisarCredito2() {
+  var compras = [2500, 1200, 800, 100];
+  var totalCompras = compras[0];
+  var limite = 5000;
+  var status = 'aprovado';
+  var saldoDisponivel = 0;
+  var i = 1;
+
+  while (limite >= totalCompras && i < compras.length) {
+      totalCompras += compras[i];
+      i++;
+  }
+
+  saldoDisponivel = limite - totalCompras;
+
+  if (saldoDisponivel < 0) {
+      status = 'negado';
+  }
+  console.log(`Seu crédito foi ${status}. Saldo disponível: ${saldoDisponivel}.`);
+}
